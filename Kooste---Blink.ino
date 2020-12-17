58a6dc2bc437dc8799c9457b04e18db22b0ec75e
janilehto <43060125+janilehto@users.noreply.github.com>
2020-12-17 08:54:59 +0200

    Kytkentä todettu toimivaksi

--- Kooste---Blink.ino
+++ Kooste---Blink.ino
@@ -1,1 +1,30 @@
+#define BIT1 13
+#define BIT2 12
+#define BIT4 11
+#define BIT8 10
+#define BITHIGH HIGH
+#define BITLOW LOW
 
+#define DISP1 5
+#define DISP2 4
+#define DISPON LOW
+#define DISPOFF HIGH
+
+void setup(){
+  pinMode(BIT1, OUTPUT);
+  pinMode(BIT2, OUTPUT);
+  pinMode(BIT4, OUTPUT);
+  pinMode(BIT8, OUTPUT);
+  pinMode(DISP1, OUTPUT);
+  pinMode(DISP2, OUTPUT);
+  digitalWrite(BIT1, BITLOW);
+  digitalWrite(BIT2, BITLOW);
+  digitalWrite(BIT4, BITLOW);
+  digitalWrite(BIT8, BITLOW);
+  digitalWrite(DISP1, DISPOFF);
+  digitalWrite(DISP2, DISPOFF);
+}
+
+void loop(){
+  digitalWrite(DISP2, DISPON);
+}
